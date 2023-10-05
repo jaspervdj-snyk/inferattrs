1f795acf742174f5dc2494bd61dbaf2fe0843b80
Jasper Van der Jeugt <jasper.vanderjeugt@snyk.io>
2023-10-05 14:35:41 -0400

    includes

--- snippet.lua
+++ snippet.lua
@@ -1,31 +1,31 @@
 string.startswith = function(self, prefix)
-    return string.sub(self, 1, string.len(prefix)) == prefix
+  return string.sub(self, 1, string.len(prefix)) == prefix
 end
 
 string.endswith = function(self, suffix)
-    return string.sub(self, -string.len(suffix), -1) == suffix
+  return string.sub(self, -string.len(suffix), -1) == suffix
 end
 
 function CodeBlock(cb)
   local file = cb.attributes.snippet
   if file then
-    local included = {}
-    local including = false
+    local snippet = {}
+    local inbraces = false
     for line in io.open("infer.go", "r"):lines() do
       if line:startswith(cb.text) then
-        included[#included + 1] = line
+        snippet[#snippet + 1] = line
         if line:endswith("{") then
-          including = true
+          inbraces = true
         end
-      elseif including then
-        included[#included + 1] = line
+      elseif inbraces then
+        snippet[#snippet + 1] = line
       end
 
       if line:startswith("}") then
-        including = false
+        inbraces = false
       end
     end
-    return pandoc.CodeBlock(table.concat(included, "\n"), cb.attr), false
+    return pandoc.CodeBlock(table.concat(snippet, "\n"), cb.attr), false
   end
   return cb, true
 end
